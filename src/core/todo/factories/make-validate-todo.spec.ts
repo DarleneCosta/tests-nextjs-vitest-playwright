7749d382d2e01e1b51917d9d7dfdacfa738c8e1b
DarleneCosta <darl_mel@hotmail.com>
2025-10-16 06:03:49 -0300

    Enhance package.json with new dotenv-based test scripts and update make-validate-todo types and tests for improved validation handling

--- src/core/todo/factories/make-validate-todo.spec.ts
+++ src/core/todo/factories/make-validate-todo.spec.ts
@@ -2,11 +2,11 @@ import * as sanitizeStringModule from "@/core/utils/sanitize-str";
 import { makeValidateTodo } from "./make-validate-todo";
 import * as validateTodoDescriptionModule from "../schemas/validate-todo-description";
 import * as makeNewTodoModule from "./make-new-todo";
+import { InvalidTodo, ValidTodo } from "./make-validate-todo";
 
 describe('makeValidateTodo unit test', () => {
     it('should call the sanitizeString function with the correct value', () => {
-       const description = 'Buy groceries'
-       const sanitizeStringSpy = vi.spyOn(sanitizeStringModule, 'sanitizeString').mockReturnValue(description);
+    const { sanitizeStringSpy, description } = makeMocks();
        //.mockReturnValue('um valor qualquer');//quando nao é uma promise   
        //.mockResolvedValue('um valor qualquer');//quando é uma promise
        //.mockRejectedValue('um valor qualquer');//quando é uma promise rejeitada
@@ -17,29 +17,63 @@ describe('makeValidateTodo unit test', () => {
         expect(sanitizeStringSpy).toHaveBeenCalledWith(description);
         expect(sanitizeStringSpy).toHaveBeenCalledTimes(1);
     });
+
     it('should call the validateTodoDescription function with the returned value from sanitizeString', () => {
-        const description = 'Buy groceries'
-        const validateTodoDescriptionSpy = vi.spyOn(validateTodoDescriptionModule, 'validateTodoDescription')
-        .mockResolvedValue({ success: true });
+        const { validateTodoDescriptionSpy, description } = makeMocks();
 
         makeValidateTodo(description);
+        
+        
         expect(validateTodoDescriptionSpy).toHaveBeenCalledWith(description);
         expect(validateTodoDescriptionSpy).toHaveBeenCalledTimes(1);
+
 
     });
 
     it('should call makeNewTodo if validateTodoDescription is successful', () => {
-        const makeNewTodoSpy = vi.spyOn(makeNewTodoModule, 'makeNewTodo');
-        const description = 'Buy groceries'
-        makeValidateTodo(description);
+        const {  description, makeNewTodoSpy } = makeMocks();
+
+        const result = makeValidateTodo(description) as ValidTodo;
+
+        console.log(result);
+
+        expect(result.data.description).toEqual(description);
+        expect(result.data.createdAt).toEqual(expect.any(Date));
+        expect(result.data.id).toEqual(expect.any(String));
         expect(makeNewTodoSpy).toHaveBeenCalledWith(description);
         expect(makeNewTodoSpy).toHaveBeenCalledTimes(1);
+
     });
 
     it('should return validateTodoDescription.errors if validateTodoDescription is not successful', () => {
-        const description = 'Buy groceries'
-        const result = makeValidateTodo(description);
-        expect(result.success).toBe(true);
+        const errors = ['Error', 'any'];
+        const { validateTodoDescriptionSpy, description } = makeMocks();
+        
+        validateTodoDescriptionSpy.mockReturnValue({ success: false, errors });
+        
+        const result = makeValidateTodo(description) as InvalidTodo;
+
+        expect(result.success).toBe(false);
+        expect(result.errors).toEqual(errors);
     });
 
 }); 
+
+const makeMocks = (description: string = 'Buy groceries') => {
+    const expectedTodo = {
+        id: expect.any(String),
+        description: description,
+        createdAt: expect.any(Date),
+    };
+    const sanitizeStringSpy = vi.spyOn(sanitizeStringModule, 'sanitizeString').mockReturnValue(description);
+    const validateTodoDescriptionSpy = vi.spyOn(validateTodoDescriptionModule, 'validateTodoDescription')
+    .mockReturnValue({ success: true });
+    const makeNewTodoSpy = vi.spyOn(makeNewTodoModule, 'makeNewTodo').mockReturnValue(expectedTodo);
+    return {
+        sanitizeStringSpy,
+        validateTodoDescriptionSpy,
+        makeNewTodoSpy,
+        description,
+        expectedTodo,
+    }
+}
