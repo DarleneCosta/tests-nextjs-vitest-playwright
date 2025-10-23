226df712b64097a05799f93f24be99d1bb7a0e22
DarleneCosta <darl_mel@hotmail.com>
2025-10-23 07:06:33 -0300

    Add test utility functions for Todo repository, including makeTestTodos and insertTestTodos, to streamline test data creation. Introduce a new SQLite database file for integration testing.

--- src/core/__tests__/utils/make-test-todo-repository.ts
+++ src/core/__tests__/utils/make-test-todo-repository.ts
@@ -1,15 +1,43 @@
 import { DrizzleTodoRepository } from '@/core/todo/repositories/drizzle-todo.repository';
 import { drizzleDatabase } from '@/db/drizzle/migrations';
-import { Todo } from '@/core/todo/schemas/todo.contract';
 import { eq } from 'drizzle-orm';
+
+export const makeTestTodos = () => {
+  return Array.from({ length: 5 }).map((_, index) => {
+    const newTodo = {
+      id: index.toString(),
+      description: `Todo ${index}`,
+      createdAt: `date ${index}`,
+    };
+
+    return newTodo;
+  });
+};
 
 export async function makeTestTodoRepository() {
   const { db, todoTable } = drizzleDatabase;
   const repository = new DrizzleTodoRepository(db);
-  const insertTodoDb = (todo: Todo) => db.insert(todoTable).values(todo);
+  const todos = makeTestTodos();
+
+  const insertTodoDb = () => db.insert(todoTable);
   const deleteTodoNoWhere = () => db.delete(todoTable);
   const deleteTodoDb = (id: string) =>
     db.delete(todoTable).where(eq(todoTable.id, id));
 
-  return { repository, insertTodoDb, deleteTodoNoWhere, deleteTodoDb };
+  return {
+    todos,
+    repository,
+    insertTodoDb,
+    deleteTodoDb,
+    deleteTodoNoWhere,
+  };
 }
+
+export const insertTestTodos = async () => {
+  const { insertTodoDb } = await makeTestTodoRepository();
+  const todos = makeTestTodos();
+
+  await insertTodoDb().values(todos);
+
+  return todos;
+};
